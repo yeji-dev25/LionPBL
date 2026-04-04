11ec2b78fc68d36c50cbdb1a63369a54324063fc
yeji-dev25 <jyjjj0329@gachon.ac.kr>
2026-04-05 01:47:18 +0900

    feat: step1 완료 - 유효성 검증 후 객체 생성

---
+++ src/week2/package1/Lion.java
@@ -0,0 +1,14 @@
+package week2.package1;
+
+public class Lion {
+
+    public String name;
+    String specialty;
+    public int corps;
+
+    public Lion(String name, String specialty, int corps) {
+        this.name = name;
+        this.specialty = specialty;
+        this.corps = corps;
+    }
+}
---
+++ src/week2/package1/step1.java
@@ -0,0 +1,26 @@
+package week2.package1;
+
+import java.util.Scanner;
+
+public class step1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        String specialty = sc.nextLine();
+        int corps = sc.nextInt();
+
+        if(name.trim().isEmpty()) {
+            throw new IllegalArgumentException("이름이 비었습니다.");
+        }
+        else if(specialty.trim().isEmpty()) {
+            throw new IllegalArgumentException("전공이 비어있습니다.");
+        }
+        else if(corps < 1) {
+            throw new IllegalArgumentException("기수가 1 미만입니다.");
+        }
+        Lion lion = new Lion(name, specialty, corps);
+
+    }
+}
